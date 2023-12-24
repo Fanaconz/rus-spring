cbd7d9234100f1826972295af9653a3768c96b54
Fanaconz <119515008+Fanaconz@users.noreply.github.com>
2023-12-25 00:26:52 +0300

    Initiaalll commit i guess

---
+++ demoon/demoon/src/main/java/ru/ruslan/demoon/DemoonApplication.java
@@ -0,0 +1,13 @@
+package ru.ruslan.demoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoonApplication.class, args);
+	}
+
+}
